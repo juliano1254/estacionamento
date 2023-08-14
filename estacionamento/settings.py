dea907974d86f75d54d39d4b44f13abb2fd38895
Juliano <juliano1254@gmail.com>
2023-08-14 09:24:33 -0400

    secret_key nas variáveis de ambiente.

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -5,7 +5,8 @@ from dj_database_url import parse as dburl
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
