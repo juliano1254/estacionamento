433518ebae17fbd65f96b7f0825195a79486bff7
Juliano <juliano1254@gmail.com>
2023-08-18 17:02:09 -0400

    Terminei aula 223.

--- website/views.py
+++ website/views.py
@@ -7,6 +7,12 @@ def servicos(request):
     return render(request, 'website/servicos.html')
 
 def contato(request):
+    #import pdb; pdb.set_trace()
+    contato = {}
+    contato['nome'] = request.POST.get('nome')
+    contato['email'] = request.POST.get('email')
+    contato['telefone'] = request.POST.get('telefone')
+    contato['mensagem'] = request.POST.get('mensagem')
     return render(request, 'website/contato.html')
 
 def sobre(request):
