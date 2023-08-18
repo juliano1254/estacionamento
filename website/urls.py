c3cfdb8de980fc53a170f1271ab65b75b2b716ee
Juliano <juliano1254@gmail.com>
2023-08-18 16:13:37 -0400

    Criado as páginas Sobre e Planos. Terminado aula 222.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from website.views import (
     home,
-    contato
+    servicos,
+    contato,
+    sobre,
+    planos
 )
 
 urlpatterns = [
     path('', home, name='website_home'),
     path('contato/', contato, name='website_contato'),
+    path('servicos/', servicos, name='website_servicos'),
+    path('sobre/', sobre, name='website_sobre'),
+    path('planos/', planos, name='website_planos'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'website/index.html')
 
+def servicos(request):
+    return render(request, 'website/servicos.html')
+
 def contato(request):
     return render(request, 'website/contato.html')
+
+def sobre(request):
+    return render(request, 'website/sobre.html')
+
+def planos(request):
+    return render(request, 'website/planos.html')
