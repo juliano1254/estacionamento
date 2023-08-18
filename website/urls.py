032ce1a36f14ec5fc3532ba61c045f8d4ba1badd
Juliano <juliano1254@gmail.com>
2023-08-18 12:39:21 -0400

    Criado a página contato.

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from website.views import (
     home,
+    contato
 )
 
 urlpatterns = [
     path('', home, name='website_home'),
+    path('contato/', contato, name='website_contato'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/index.html')
+
+def contato(request):
+    return render(request, 'website/contato.html')
